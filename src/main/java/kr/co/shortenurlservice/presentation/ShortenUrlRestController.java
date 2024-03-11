9465dbe8ca574719827fc36033131c798cadb051
DDunDDang <thdwns0331@gmail.com>
2024-03-11 14:18:07 +0900

    기능 구현: 단축 URL 생성

--- src/main/java/kr/co/shortenurlservice/presentation/ShortenUrlRestController.java
+++ src/main/java/kr/co/shortenurlservice/presentation/ShortenUrlRestController.java
@@ -1,25 +1,34 @@
 package kr.co.shortenurlservice.presentation;
 
+import jakarta.validation.Valid;
+import kr.co.shortenurlservice.application.SimpleShortenUrlService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class ShortenUrlRestController {
 
+    private SimpleShortenUrlService simpleShortenUrlService;
+
+    @Autowired
+    public ShortenUrlRestController(SimpleShortenUrlService simpleShortenUrlService) {
+        this.simpleShortenUrlService = simpleShortenUrlService;
+    }
+
     @RequestMapping(value = "/shorten-url", method = RequestMethod.POST)
-    public ResponseEntity<?> createShortenUrl() {
-        return ResponseEntity.ok().body(null);
+    public ResponseEntity<ShortenUrlCreateResponseDto> createShortenUrl(@Valid @RequestBody ShortenUrlCreateRequestDto shortenUrlCreateRequestDto) {
+        ShortenUrlCreateResponseDto shortenUrlCreateResponseDto = simpleShortenUrlService.generateShortenUrl(shortenUrlCreateRequestDto);
+        return ResponseEntity.ok(shortenUrlCreateResponseDto);
     }
 
     @RequestMapping(value = "/{shorten-url-key}", method = RequestMethod.GET)
-    public ResponseEntity<?> redirectShortenUrl() {
+    public ResponseEntity<?> redirectShortenUrl(@PathVariable("shorten-url-key") String shortenUrlKey) {
         return ResponseEntity.ok().body(null);
     }
 
     @RequestMapping(value = "/shorten-url/{shorten-url-key}", method = RequestMethod.GET)
-    public ResponseEntity<?> getShortenUrlInformation() {
+    public ResponseEntity<ShortenUrlInformationDto> getShortenUrlInformation(@PathVariable("shorten-url-key") String shortenUrlKey) {
         return ResponseEntity.ok().body(null);
     }
 }
