7f1fbc8749d811db811346bf442b1594efcd53b9
DDunDDang <thdwns0331@gmail.com>
2024-03-11 14:54:39 +0900

    예외: 단축 URL을 찾지 못함

--- src/main/java/kr/co/shortenurlservice/application/SimpleShortenUrlService.java
+++ src/main/java/kr/co/shortenurlservice/application/SimpleShortenUrlService.java
@@ -1,5 +1,6 @@
 package kr.co.shortenurlservice.application;
 
+import kr.co.shortenurlservice.domain.NotFoundShortenUrlException;
 import kr.co.shortenurlservice.domain.ShortenUrl;
 import kr.co.shortenurlservice.domain.ShortenUrlRepository;
 import kr.co.shortenurlservice.presentation.ShortenUrlCreateRequestDto;
@@ -33,6 +34,8 @@ public class SimpleShortenUrlService {
     public String getOriginalUrlByShortenUrlKey(String shortenUrlKey) {
         ShortenUrl shortenUrl = shortenUrlRepository.findShortenUrlByShortenUrlKey(shortenUrlKey);
 
+        if (null == shortenUrl) throw new NotFoundShortenUrlException();
+
         shortenUrl.increaseRedirectCount();
         shortenUrlRepository.saveShortenUrl(shortenUrl);
 
@@ -43,6 +46,8 @@ public class SimpleShortenUrlService {
 
     public ShortenUrlInformationDto getShortenUrlInformationByShortenUrlKey(String shortenUrlKey) {
         ShortenUrl shortenUrl = shortenUrlRepository.findShortenUrlByShortenUrlKey(shortenUrlKey);
+
+        if (null == shortenUrl) throw new NotFoundShortenUrlException();
 
         ShortenUrlInformationDto shortenUrlInformationDto = new ShortenUrlInformationDto(shortenUrl);
 
